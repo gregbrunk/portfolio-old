c4dbe92b2b414beed8698a2428490404940206e3
Greg Brunk <greg.brunk@gmail.com>
2016-08-21 11:07:21 -0600

    Integrates front-end routing

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,17 @@
-angular.module('Portfolio', ['ngRoute']);
+var app = angular.module('Portfolio', ['ngRoute']);
+
+app.config(function($routeProvider, $locationProvider){
+    $routeProvider
+    .when('/', {
+      templateUrl: '/templates/project-index.html',
+      controller: 'ProjectIndexController'
+    })
+    .when('/projects/:id', { 
+    templateUrl: 'templates/project-show.html',
+    controller: 'ProjectShowController'
+  });
+    // $locationProvider.html5Mode({
+    //   enabled: true,
+    //   requireBase: false
+    // });
+});
--- public/js/controllers/projectController.js
+++ public/js/controllers/projectController.js
@@ -13,6 +13,6 @@ function ProjectShowController($scope, $http, $location) {
   var path = $location.absUrl();
   var split = path.split('/');
   var id = split[split.length-1];
-  $http.get("http://localhost:3000/api/cards/" + id)
+  $http.get("http://localhost:3000/api/projects/" + id)
     .then(function(response){ $scope.project = response.data; });
 }
