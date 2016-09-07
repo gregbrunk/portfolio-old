f1f83fb55bd497f368a02a821ab47116488b5c64
Greg Brunk <greg.brunk@gmail.com>
2016-09-07 12:49:40 -0600

    preps model export for heroku deploy

--- models/index.js
+++ models/index.js
@@ -1,4 +1,4 @@
 var mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/portfolio");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/portfolio");
 
 module.exports.Project = require("./project.js");
